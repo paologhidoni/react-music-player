f2ef8fa5b984ec05a24d4875526b016584729090
Paolo <paolo.ghidoni82@gmail.com>
2021-12-09 14:04:25 +0000

    Style player, store songs and assets

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import "../styles/Song.css"
+import "../styles/modifiers.css";
+import "../styles/Song.css";
 
 const Song = ({currentSong}) => {
   return (
-    <div className="song-container">
+    <div className="song-container stack-md">
       <img src={currentSong.cover} alt="song cover"></img>
       <h2>{currentSong.name}</h2>
-      <h2>{currentSong.artist}</h2>
+      <h3>{currentSong.artist}</h3>
 
     </div>
   )
